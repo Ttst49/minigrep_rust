1814bc8ab55143a3100912287da59e5549a5a996
Ttst <thibautstachnick@gmail.com>
2023-12-27 20:34:46 +0100

    make a new function in order to add logic into it and remove it from main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
-    let search_arg = &args[1];
-    let file_name = &args[2];
+
+    let (search_arg, file_name) = interpreter_config(&args);
 
     println!("On recherche : {}", search_arg);
     println!("Dans le fichier : {}", file_name);
+
+    let content = fs::read_to_string(file_name)
+        .expect("Quelque chose a échoué");
+
+    println!("le contenu de {} est : '{}'",file_name,content)
+}
+
+
+fn interpreter_config(args : &[String])->(&str, &str){
+    let search_arg = &args[1];
+    let file_name = &args[2];
+
+    (search_arg, file_name)
 }
