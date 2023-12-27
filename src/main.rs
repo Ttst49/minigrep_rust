c5836db9e182794c875c9fa7854908a8d9744d3c
Ttst <thibautstachnick@gmail.com>
2023-12-27 20:21:35 +0100

    base keeping args in a vector and use them as search_arg and name_file for the grep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+    let search_arg = &args[1];
+    let file_name = &args[2];
+
+    println!("On recherche : {}", search_arg);
+    println!("Dans le fichier : {}", file_name);
 }
