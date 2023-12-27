da8a76f354af83ba619d6f6adc40f4710032b1f9
Ttst <thibautstachnick@gmail.com>
2023-12-27 21:41:00 +0100

    add a use and the name of the app to make it work with lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+use minigrep::Config;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
@@ -14,36 +13,9 @@ fn main() {
     println!("On recherche : {}", config.search_arg);
     println!("Dans le fichier : {}", config.file_name);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Erreur applicative : {}", e);
 
         process::exit(1);
     }
-}
-
-struct Config{
-    search_arg: String,
-    file_name: String
-}
-
-impl Config {
-    fn new(args : &[String])->Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Il n'y a pas assez d'arguments")
-        }
-
-        let search_arg = args[1].clone();
-        let file_name = args[2].clone();
-
-        Ok(Config{search_arg, file_name})
-    }
-}
-
-fn run( config: Config)-> Result<(),Box<dyn Error>>{
-    let content = fs::read_to_string(config.file_name)?;
-
-
-    println!("le contenu est : '{}'",content);
-
-    Ok(())
 }
