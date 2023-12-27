d7c882b0124a64488064102146d3e18846e6d1cd
Ttst <thibautstachnick@gmail.com>
2023-12-27 21:31:39 +0100

    add run function that launch logical inside main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
 
-    let (search_arg, file_name) = interpreter_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problème rencontré lors de l'interprétation des arguments : {}", err);
+        process::exit(1);
+    });
 
-    println!("On recherche : {}", search_arg);
-    println!("Dans le fichier : {}", file_name);
+    println!("On recherche : {}", config.search_arg);
+    println!("Dans le fichier : {}", config.file_name);
 
-    let content = fs::read_to_string(file_name)
-        .expect("Quelque chose a échoué");
+    if let Err(e) = run(config) {
+        println!("Erreur applicative : {}", e);
 
-    println!("le contenu de {} est : '{}'",file_name,content)
+        process::exit(1);
+    }
 }
 
 struct Config{
@@ -20,9 +26,24 @@ struct Config{
     file_name: String
 }
 
-fn interpreter_config(args : &[String])->Config{
-    let search_arg = args[1].clone();
-    let file_name = args[2].clone();
+impl Config {
+    fn new(args : &[String])->Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Il n'y a pas assez d'arguments")
+        }
 
-    Config{search_arg, file_name}
+        let search_arg = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Config{search_arg, file_name})
+    }
+}
+
+fn run( config: Config)-> Result<(),Box<dyn Error>>{
+    let content = fs::read_to_string(config.file_name)?;
+
+
+    println!("le contenu est : '{}'",content);
+
+    Ok(())
 }
